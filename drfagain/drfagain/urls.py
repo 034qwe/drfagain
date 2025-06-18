d3f64a3b72c1f62b91adf5a69c29b74ec71cd39d
bohdan <johndoe@example.com>
2025-06-18 17:36:05 +0300

    add base serializer

--- drfagain/drfagain/urls.py
+++ drfagain/drfagain/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import AnimalsAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/animals/', AnimalsAPIView.as_view())
 ]
