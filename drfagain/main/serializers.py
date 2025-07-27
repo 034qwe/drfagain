92341c3b2f1fced3e63b9c0890c68392b47c46bb
bohdan <johndoe@example.com>
2025-07-27 23:27:03 +0300

    create my own permission classes and add their into  views

--- drfagain/main/serializers.py
+++ drfagain/main/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Animals
 
 class AnimalSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
         model = Animals     
-        fields = '__all__'
+        fields = '__all__' 
--- drfagain/main/views.py
+++ drfagain/main/views.py
@@ -3,31 +3,33 @@ from rest_framework import generics
 from .models import Animals, Category_Animals
 from .serializers import AnimalSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from .mypermissions import *
 
-# class AnimalsAPIView(generics.ListCreateAPIView):
-#     queryset = Animals.objects.all()
-#     serializer_class = AnimalSerializer
-
-
-# class AnimalAPIUpdate(generics.UpdateAPIView):
-#     queryset = Animals.objects.all()
-#     serializer_class = AnimalSerializer
-
-# class AnimalAPIMore(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Animals.objects.all()
-#     serializer_class = AnimalSerializer
-
-
-class AnimalViewSet(viewsets.ModelViewSet):
-    # queryset = Animals.objects.all()
-    def get_queryset(self):
-        pk = self.kwargs.get('pk')
-        if pk:
-            return Animals.objects.filter(pk=pk)
-        else:
-            return Animals.objects.all()
-        
-            
-            
-            
+class AnimalsAPIView(generics.ListCreateAPIView):
+    queryset = Animals.objects.all()
     serializer_class = AnimalSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class AnimalAPIUpdate(generics.UpdateAPIView):
+    queryset = Animals.objects.all()
+    serializer_class = AnimalSerializer
+    permission_classes = (IsOwnerOrReadOnly)
+
+class AnimalAPIDestroy(generics.DestroyAPIView):
+    queryset = Animals.objects.all()
+    serializer_class = AnimalSerializer
+
+
+# class AnimalViewSet(viewsets.ModelViewSet):
+#     # queryset = Animals.objects.all()
+#     def get_queryset(self):
+#         pk = self.kwargs.get('pk')
+#         if pk:
+#             return Animals.objects.filter(pk=pk)
+#         else:
+#             return Animals.objects.all()
+
+
+#     serializer_class = AnimalSerializer
