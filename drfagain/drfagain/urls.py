ad2efa569f4f0c330b0b4b0e04c110ad68944ea4
bohdan <johndoe@example.com>
2025-07-27 23:25:39 +0300

    split viewset

--- drfagain/drfagain/urls.py
+++ drfagain/drfagain/urls.py
@@ -1,31 +1,18 @@
-"""
-URL configuration for drfagain project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from main.views import *
 from rest_framework import routers
 
+
 router = routers.SimpleRouter()
-router.register('animal',AnimalViewSet, basename='animal')
+# router.register('animal',AnimalViewSet, basename='animal')
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    # path('animals/', AnimalViewSet.as_view({'get':'list'})),
-    # path('animals/<int:pk>/', AnimalViewSet.as_view({'put':'update'})),
-    path('', include(router.urls))
+    path('animals/', AnimalsAPIView.as_view()),
+    path('animals/<int:pk>/', AnimalAPIUpdate.as_view()),
+    path('animalsdelete/<int:pk>/', AnimalAPIDestroy.as_view()),
+    # path('', include(router.urls))
 
  ]
