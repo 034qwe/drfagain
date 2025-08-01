6282c12073aa0cbb3f5801c7b9df8990a059f78b
bohdan <johndoe@example.com>
2025-08-01 21:26:06 +0300

    try jwt token

--- drfagain/drfagain/urls.py
+++ drfagain/drfagain/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from main.views import *
 from rest_framework import routers
+from rest_framework_simplejwt.views import  TokenObtainPairView, TokenRefreshView,  TokenVerifyView
 
 
 router = routers.SimpleRouter()
@@ -16,6 +17,10 @@ urlpatterns = [
     path('animalsdelete/<int:pk>/', AnimalAPIDestroy.as_view()),
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),  JWT TOken auth
+
     # path('', include(router.urls))
 
  ]
